a90e8de60b8a886ba068e0e1db9449e68bea8259
Berend Nannes <berend.nannes@quicknet.nl>
2017-02-27 17:50:51 +0100

    Updated json (1)

--- Homework/Week 4/map.js
+++ Homework/Week 4/map.js
@@ -1,18 +1,24 @@
-var map = new Datamap({
-	element: document.getElementById('container'),
-	geographyConfig: {
-		//highlightOnHover: false,
-		popupTemplate: function(geo, data) {
-			return ['<div class="hoverinfo">',
-			geo.properties.name,
-			'</div>'].join('');
-            },
-		borderColor: 'black',
-		borderWidth: 0.5,
-	},
-		fills: {
-		defaultFill: 'white'
-	},
-	data: {
-	}
+
+d3.json("https://raw.githubusercontent.com/BerendNannes/DataProcessing/master/Homework/Week%204/globalization.json", function(globalizationData) {
+	// json request
+	
+	console.log(globalizationData);//json.parse()
+	
+	var map = new Datamap({
+		element: document.getElementById('container'),
+		geographyConfig: {
+			popupTemplate: function(geo, data) {
+				console.log(data.index);
+				return ['<div class="hoverinfo">',
+				geo.properties.name, data.index,
+				'</div>'].join('');
+				},
+			borderColor: 'black',
+			borderWidth: 0.5,
+		},
+			fills: {
+			defaultFill: 'white'
+		},
+		data: stuff
+	});
 });
