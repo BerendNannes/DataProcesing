e49e3d7a77571492bfde8cb6fa5dcab2cad3e17f
Berend Nannes <berend.nannes@quicknet.nl>
2017-02-28 10:39:08 +0100

    Homework 4

--- Homework/Week 4/map.js
+++ Homework/Week 4/map.js
@@ -1,25 +1,57 @@
-
+/*
+map.js
+Data Processing
+Berend Nannes
+*/
 
 d3.json("https://raw.githubusercontent.com/BerendNannes/DataProcessing/master/Homework/Week%204/globalization.json", function(globalizationData) {
-	// json request
 	
-	console.log(globalizationData);
+	// retrieve json data
+	dataset = globalizationData[0];
 	
+	// get min and max values
+	var indexValues = Object.keys(dataset).map(function (key) { return dataset[key].index; } );
+	var minIndex = Math.min.apply(null, indexValues);
+	var maxIndex = Math.max.apply(null, indexValues);
+	
+	// create color scale
+	var paletteScale = d3.scale.linear()
+		.domain([minIndex, maxIndex])
+		.range(["#ffe9d8","#7a3501"]);
+	
+	// set fillColor to data points
+	for (var i in dataset) {
+		dataset[i].fillColor = paletteScale(dataset[i].index);
+	}
+	
+	// create map
 	var map = new Datamap({
 		element: document.getElementById('container'),
+		projection: "mercator",
 		geographyConfig: {
 			popupTemplate: function(geo, data) {
-				console.log(data.index);
-				return ['<div class="hoverinfo">',
-				geo.properties.name,
+				
+				// check if there's available data
+				if (data) {output = "Globalisation index: " + data.index;}
+				else {output = "No data available";}
+				
+				// return hover info
+				return ['<div class="hoverinfo"><strong>',
+				geo.properties.name, '</strong></br>' , output,
 				'</div>'].join('');
 				},
+				
+			// style properties
 			borderColor: 'black',
 			borderWidth: 0.5,
+			highlightFillColor: 'black',
+			highlightBorderColor: 'black',
 		},
+		// set default fill color
 		fills: {
 			defaultFill: 'white'
 		},
-		//data: stuff
+		// set data
+		data: dataset
 	});
 });
--- README.md
+++ README.md
@@ -6,12 +6,16 @@ Homework:
         <td>Week 1: Scraping IMDB</td>
 		<td>N/A</td>
     </tr>
-	    <tr>
+	<tr>
         <td>Week 2: JavaScript line graph</td>
 		<td><a href="http://rawgit.com/BerendNannes/DataProcessing/master/Homework/Week%202/index.html">index.html</a></td>
     </tr>
-	    <tr>
+	<tr>
         <td>Week 3: D3 Barchart</td>
 		<td><a href="http://rawgit.com/BerendNannes/DataProcessing/master/Homework/Week%203/barchart.html">barchart.html</a></td>
+    </tr>
+	<tr>
+        <td>Week 4: D3 Map</td>
+		<td><a href="http://rawgit.com/BerendNannes/DataProcessing/master/Homework/Week%204/map.html">map.html</a></td>
     </tr>
 </table>
